d0d87952ffbc1d92e3b0d187fb293f83c3e359cd
dexmbachu <xeddtech@gmail.com>
2019-11-17 15:34:45 +0100

    updated db schema

--- kidstarmodels.sql
+++ kidstarmodels.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 15, 2019 at 04:03 PM
--- Server version: 5.7.26
--- PHP Version: 7.3.5
+-- Generation Time: Nov 17, 2019 at 02:29 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -64,14 +64,19 @@ CREATE TABLE IF NOT EXISTS `applications` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `applications_image_id_index` (`image_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `applications`
 --
 
 INSERT INTO `applications` (`id`, `image_id`, `surname`, `othernames`, `age`, `health_issues`, `health_details`, `nationality`, `state`, `city`, `address`, `vital_state`, `school_name`, `school_class`, `height`, `bust`, `waist`, `hips`, `question1`, `question2`, `question3`, `question4`, `question5`, `parent_surname`, `parent_othernames`, `parent_mobile`, `parent_email`, `parent_address`, `payment_details`, `paid`, `created_at`, `updated_at`) VALUES
-(1, 3, 'Neutron', 'Neutron', 4, 0, 'ttttt', 'nigeria', 'georgia', 'rivers', 'tttttttttt', 'rrr', 'school name', 'school class', 'heught', 'bust', 'waist', 'hips', 'dddddddddddd', 'dddddddddd', 'ddddddddddddddd', 'ddddddddddddddd', 'dddddddddd', 'ddddddd', 'dddddd', 'ddddddd', 'www@email.com', 'wwwwwwww', 'wwwwwwwww', 0, '2019-11-15 11:18:17', '2019-11-15 11:18:17');
+(1, 3, 'Neutron', 'Neutron', 4, 0, 'ttttt', 'nigeria', 'georgia', 'rivers', 'tttttttttt', 'rrr', 'school name', 'school class', 'heught', 'bust', 'waist', 'hips', 'dddddddddddd', 'dddddddddd', 'ddddddddddddddd', 'ddddddddddddddd', 'dddddddddd', 'ddddddd', 'dddddd', 'ddddddd', 'www@email.com', 'wwwwwwww', 'wwwwwwwww', 1, '2019-11-15 11:18:17', '2019-11-17 12:45:44'),
+(2, 4, 'Xeddddd', 'xedddd', 4, 1, 'ffff', 'nigeria', 'georgia', 'rivers', 'address', 'rrr', 'school name', 'school class', 'heught', 'bust', 'waist', 'hips', 'dddddddddddd', 'dddddddddd', 'ddddddddddddddd', 'ddddddddddddddd', 'dddddddddd', 'ddddddd', 'dddddd', 'ddddddd', 'www@email.com', 'wwwwwwww', 'wwwwwwwww', 1, '2019-11-17 12:30:51', '2019-11-17 12:45:48'),
+(3, 5, 'Xeddddd', 'xedddd', 4, 1, 'ffff', 'nigeria', 'georgia', 'rivers', 'address', 'rrr', 'school name', 'school class', 'heught', 'bust', 'waist', 'hips', 'dddddddddddd', 'dddddddddd', 'ddddddddddddddd', 'ddddddddddddddd', 'dddddddddd', 'ddddddd', 'dddddd', 'ddddddd', 'www@email.com', 'wwwwwwww', 'wwwwwwwww', 0, '2019-11-17 12:33:52', '2019-11-17 12:33:52'),
+(4, 6, 'Xeddddd', 'xedddd', 4, 1, 'ffff', 'nigeria', 'georgia', 'rivers', 'address', 'rrr', 'school name', 'school class', 'heught', 'bust', 'waist', 'hips', 'dddddddddddd', 'dddddddddd', 'ddddddddddddddd', 'ddddddddddddddd', 'dddddddddd', 'ddddddd', 'dddddd', 'ddddddd', 'www@email.com', 'wwwwwwww', 'wwwwwwwww', 0, '2019-11-17 12:36:23', '2019-11-17 12:36:23'),
+(5, 7, 'Xeddddd', 'xedddd', 4, 1, 'ffff', 'nigeria', 'georgia', 'rivers', 'address', 'rrr', 'school name', 'school class', 'heught', 'bust', 'waist', 'hips', 'dddddddddddd', 'dddddddddd', 'ddddddddddddddd', 'ddddddddddddddd', 'dddddddddd', 'ddddddd', 'dddddd', 'ddddddd', 'www@email.com', 'wwwwwwww', 'wwwwwwwww', 0, '2019-11-17 12:38:40', '2019-11-17 12:38:40'),
+(6, 8, 'Xeddddd', 'xedddd', 4, 1, 'ffff', 'nigeria', 'georgia', 'rivers', 'address', 'rrr', 'school name', 'school class', 'heught', 'bust', 'waist', 'hips', 'dddddddddddd', 'dddddddddd', 'ddddddddddddddd', 'ddddddddddddddd', 'dddddddddd', 'ddddddd', 'dddddd', 'ddddddd', 'www@email.com', 'wwwwwwww', 'wwwwwwwww', 0, '2019-11-17 12:41:16', '2019-11-17 12:41:16');
 
 -- --------------------------------------------------------
 
@@ -103,7 +108,7 @@ CREATE TABLE IF NOT EXISTS `images` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `images`
@@ -112,7 +117,12 @@ CREATE TABLE IF NOT EXISTS `images` (
 INSERT INTO `images` (`id`, `img`, `created_at`, `updated_at`) VALUES
 (1, '1573819367AdobeStock_35124379_Preview.jpeg', '2019-11-15 11:02:47', '2019-11-15 11:02:47'),
 (2, '1573819968AdobeStock_35124379_Preview.jpeg', '2019-11-15 11:12:49', '2019-11-15 11:12:49'),
-(3, '1573820296AdobeStock_35124379_Preview.jpeg', '2019-11-15 11:18:16', '2019-11-15 11:18:16');
+(3, '1573820296AdobeStock_35124379_Preview.jpeg', '2019-11-15 11:18:16', '2019-11-15 11:18:16'),
+(4, '15739974505797a92bdd0895057c8b4a67-750-563.jpg', '2019-11-17 12:30:50', '2019-11-17 12:30:50'),
+(5, '15739976325797a92bdd0895057c8b4a67-750-563.jpg', '2019-11-17 12:33:52', '2019-11-17 12:33:52'),
+(6, '15739977835797a92bdd0895057c8b4a67-750-563.jpg', '2019-11-17 12:36:23', '2019-11-17 12:36:23'),
+(7, '15739979205797a92bdd0895057c8b4a67-750-563.jpg', '2019-11-17 12:38:40', '2019-11-17 12:38:40'),
+(8, '15739980765797a92bdd0895057c8b4a67-750-563.jpg', '2019-11-17 12:41:16', '2019-11-17 12:41:16');
 
 -- --------------------------------------------------------
 
